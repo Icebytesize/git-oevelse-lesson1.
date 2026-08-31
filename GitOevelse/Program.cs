486c27ffa6b455ad4f2827342dd616e3b8a33aca
IceByteSize <Matthiasuk@gmail.com>
2026-08-31 08:23:51 +0200

    commits til branch der spørg efter navn

--- GitOevelse/Program.cs
+++ GitOevelse/Program.cs
@@ -5,3 +5,9 @@ Console.WriteLine("Tryk på en tast for at afslutte...");
 Console.ReadLine();
 
 Console.WriteLine("Hej, jeg skal lave et par flere commits");
+Console.WriteLine("Hola, dette er en commit til min branch");
+Console.ReadLine();
+
+Console.WriteLine("Hvad er dit navn: ");
+string navn = Console.ReadLine();
+Console.WriteLine("Hej " + navn + ", nice to meet you!");
